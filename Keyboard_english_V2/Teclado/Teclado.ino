b82d718f212b4df814e3402b7cd08a6302255f86
Javier Fidalgo Rodríguez <184210474+jfrNebus@users.noreply.github.com>
2024-11-19 14:44:07 +0100

    Update Teclado.ino

--- Keyboard_english_V2/Teclado/Teclado.ino
+++ Keyboard_english_V2/Teclado/Teclado.ino
@@ -2,27 +2,27 @@
 
 //----------- Matrix push buttons
 int pin1 = 2;
-int pin2 = 3;
-int pin3 = 4;
-int pin5 = 5;
-int pin6 = 6;
-int pin7 = 7;
-int pin8 = 8;
-//----------- No matrix push button, button *
-int pin4 = 10;
-int pin9 = 9;
+int pin2 = 3;#include <EEPROM.h>
+
+//----------- Matrix push buttons
+int pin1 = 12;
+int pin2 = 11;
+int pin3 = 10;
+int pin4 = 9;  //No matrix push button, button *
+int pin5 = 8;
+int pin6 = 7;
+int pin7 = 6;
+int pin8 = 5;
+int pin9 = 4;  //No matrix push button, button *
 //----------- Electromagnetic bolt
-int magnet1 = A1;
-//----------- Leds
-int positive = 11;
-int green = A5;
-int orange = A4;
-int red = A3;
-/*Leds will turn on if we set  pinMode(red, OUTPUT);, since all led are common positive pin, when we set
-their negative to output, since output sets a LOW value, conected to ground, we close the circuit.
-*/
-
-
+int magnet1 = A5;
+//----------- 
+//Leds. Each one of these pins are connected to a transistor on its base, to control each
+//LED individually.
+int green = A4;
+int orange = A3;
+int red = A2;
+//-----------
 int keyColumn; //Bound for first dimension in multidimensional array.
 int keyRow; //Bound for second dimension in multidimensional array.
 boolean keyDetected = false; //Boolean that we will use to know if we pressed any key.
@@ -41,7 +41,6 @@ which is 1; the second number of password will be save in the position 2; and so
 
 To be tested: remove this array. Instead use the for loop variable as address. This way the first
 position would be 0, and the last one would be 5.
-
 */
 byte newPasswordCounter = 0; //-TOCHECK TOCHECK TOCHECK TOCHECK TOCHECK TOCHECK TOCHECK TOCHECK TOCHECK
 byte numberOfDigits = 6; /*Number of digits of our password. This variable will set the amount of
@@ -52,7 +51,6 @@ const byte R = 3;//rows
 bool match = false; /*Boolean that will allow to control different contionals in the code based
 on whether the entered password was correct or not.
 */
-
 //Columns and rows store the pins according to the electronics schematic.
 int columns[C] = {pin5, pin6, pin7, pin8};
 int rows[R] = {pin1, pin2, pin3};
@@ -65,25 +63,18 @@ int mainMenuVariable = HIGH; /*This variable is where we will store the reading 
 value is high. * button is multipurpose, it will help us to either leave password menu without making
 any change, to cancel a wrong input, etc. This conditions will be met once mainMenuVariable == LOW.
 */
-
 void setup() {
-  //Leds
-  //We start setting "positive" pin as output and setting a low power level by analogWrite.
-  pinMode(positive, OUTPUT);
-  analogWrite(positive, 20);
-  /*No matrix push button, buttom equal to *.
-  We set one of the pin of the button as output low, and the other one as input high.
-  THE BASIC WORKING IDEA is that through pin4 you send a low pulse, and you read it in pin 9
-  which is high. You set conditionals according to pin 9: if pin9 == to low, since it was high,
-  then do this.
-  */
+  //----------- Pin setup for * button.
   pinMode(pin4, OUTPUT);
   digitalWrite(pin4, LOW);
   pinMode(pin9, INPUT);
   digitalWrite(pin9, HIGH);
-  //magnet1 is the pin that will trigger the gate pin, of the mosfet transistor.
+  //----------- Pin setup for all the transistors.
   pinMode(magnet1, OUTPUT);
-  //We start the serial monitor, to print the in process messages.
+  pinMode(green, OUTPUT);
+  pinMode(orange, OUTPUT);
+  pinMode(red, OUTPUT);
+  //-----------
   Serial.begin(9600);
   /*Now we set the values for each pin combination. According to the keyboard connexions schematic, 
   the conexion between pin1 and pin5 belongs to button number 1. Since columns array's members are: 
@@ -95,8 +86,16 @@ void setup() {
   button, but, since we are working with int arrays, we represent it with number 11. We are not using
   # key at all tho.
   */
-  result[0][0] = 1; result[0][1] = 2; result[0][2] = 3; result[1][0] = 4; result[1][1] = 5;
-  result[1][2] = 6; result[2][0] = 7; result[2][1] = 8; result[2][2] = 9; result[3][1] = 0;
+  result[0][0] = 1;
+  result[0][1] = 2;
+  result[0][2] = 3;
+  result[1][0] = 4;
+  result[1][1] = 5;
+  result[1][2] = 6;
+  result[2][0] = 7;
+  result[2][1] = 8;
+  result[2][2] = 9;
+  result[3][1] = 0;
   result[3][2] = 11;
   pinSetUp(columns, rows); //We call pinSetUp void, the one that will set up all pins. Check pinStUp void for
   //deeper explanations.
@@ -110,41 +109,40 @@ void loop() {
   /*Main loop of the system is based on mainMenuVariable. This variable is the one that sets us "inside password change"
   menu or not. If this variable is equals to low, then we get in the menu to change password, otherwise we will just
   load the confirmation void, and then the openClose void. Confirmation menu will check if what we typed is equal to
-  actual value of Eeprom memory. If both passwords are equals, it will set match = true. openClose void will either send 
-  a high pulse through A1 to the gate of mosfet and will trigger a green led for 2 seconds; or it will blink twice red 
-  light if password typed was wrong, according to the value of "match" variable set by "confirmation".
+  actual value of Eeprom memory. If both passwords are equals, it will set match = true. openClose, according to the value 
+  of "match" variable set by "confirmation" void, will send a high pulse through A1 to the gate of mosfet and will trigger
+  a green led for 2 seconds; or red LED will blink twice if the password typed was wrong.
   In the other hand we have the new password menu. If mainMenuVariable == LOW, we get in this menu. This means that
   confirmation menu was loaded at least once, and it detected that * key was pressed. Since * was pressed, mainMenuVariable
   changed from HIGH to LOW. Once inside this conditional, we will start by turning on the red LED constantly till we leave
-  this menu. This it meant to help the user to know which part of the code is being executed all the time. Then we send
+  this menu. This is meant to help the user to know which part of the code is being executed all the time. Then we send
   a message to serial monitor, we set insideNewPasswordMenu as true, and mainMenuVariable to HIGH again. We will
   talk deeply about insideNewPasswordMenu variable, in newPassword void.
   We set mainMenuVariable again to HIGH because that way, if we leave the new password menu, we wont get again
-  inside this menu, we will get just in the main loop. Then, orange led blinks 2, visual purpose only.
+  inside this menu, we will just get in the main loop. Then, orange led blinks 2, visual purpose only.
   In order to reach the "newPassword" void, a right password must be registered by "confirmation". Confirmation void will 
   set match to true or false. After loading confirmation void, and if insideNewPasswordMenu is still true, (it can change during
-  confirmation void), we load newPassword void. Once this void is over, we set red LED as input, and we leave new password menu.
+  confirmation void), we load newPassword void. Once this void is over, we turn off red LED, and we leave new password menu.
   */
 
 
   Serial.println("Main Menu.");
   if (mainMenuVariable == LOW) {
-    pinMode(red, OUTPUT);
+    analogWrite(red, 255);
     Serial.println("Inside new password conditional.");
     mainMenuVariable = HIGH;
     insideNewPasswordMenu = true;
-    pinMode(orange, OUTPUT);
+    analogWrite(orange, 255);
     delay(100);
-    pinMode(orange, INPUT);
+    analogWrite(orange, 0);
     delay(100);
-    pinMode(orange, OUTPUT);
-    delay(100);
-    pinMode(orange, INPUT);
-    delay(100);
+    analogWrite(orange, 255);
+    delay(1000);
+    analogWrite(orange, 0);
     confirmation (password, numberOfDigits, columns, rows);
     if (insideNewPasswordMenu)
       newPassword (password, numberOfDigits, columns, rows);
-    pinMode(red, INPUT);
+    analogWrite(red, 0);
   }
   else {
     confirmation (password, numberOfDigits, columns, rows);
